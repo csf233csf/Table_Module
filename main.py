231897c37ce486e7277a9793fd59f03ccadfa440
csf233csf <2452864281x@gmail.com>
2024-03-06 14:48:17 +0800

    innit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from classification import TableProcessor
+from classification import TableProcessor # 导入TableProcessor
 from tqdm import tqdm
 import os
 import glob
@@ -7,17 +7,29 @@ import glob
 input_folder = 'Table Data'
 output_folder = '表格分类3-6 run1/'
 
-foldertest = 'Table Test Runs'
-
 TableProcessor = TableProcessor()
 
 def Run_One_Folder():
     for filename in tqdm(os.listdir(input_folder), desc=f"Processing HTML File"):
         if filename.endswith('.html'):
             input_html = os.path.join(input_folder, filename)
-            TableProcessor.extract_tables(input_html,output_folder, filename)
+            TableProcessor.extract_tables(input_html, output_folder, filename)
             TableProcessor.output_statistics(output_folder, filename)
-            TableProcessor.output_debug(output_folder,filename)
+            TableProcessor.output_debug(output_folder, filename)
 
-# 开始跑路径下所有的文件
 Run_One_Folder()
+
+'''
+extract_tables(html_file_path, output_folder, filename)
+参数: html_file_path: 字符串，代表HTML文件的路径。
+     output_folder: 字符串，代表输出文件夹的路径。
+     filename: 字符串，代表HTML文件的文件名。
+
+output_statistics(output_folder, filename)
+参数: output_folder: 字符串，代表输出文件夹的路径。
+     filename: 字符串，代表HTML文件的文件名。
+
+output_debug(output_folder, filename)
+参数: output_folder: 字符串，代表输出文件夹的路径。
+     filename: 字符串，代表HTML文件的文件名。
+'''
